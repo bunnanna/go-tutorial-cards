7fdcabc9db065a6548f98fc6610dca96ee3611f1
bunnana <na.nuttanont@gmail.com>
2023-12-21 05:46:08 +0700

    feat: :sparkles: r/w to file

--- deck.go
+++ deck.go
@@ -1,31 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
 func (d deck) print() {
 	for _, card := range d {
 
-		fmt.Println( card)
+		fmt.Println(card)
 	}
 }
 
 func newDeck() deck {
 	d := deck{}
-	cardSuits := []string{"Diamonds","Sprades","Hearts","Clubs"}
-	cardNums := []string{"A","2","3","4","5","6","7","8","9","10","J","Q","K"}
-	for _,cardSuit := range cardSuits{
-		for _,cardNum := range cardNums{
+	cardSuits := []string{"Diamonds", "Sprades", "Hearts", "Clubs"}
+	cardNums := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+	for _, cardSuit := range cardSuits {
+		for _, cardNum := range cardNums {
 			d = append(d, cardNum+" of "+cardSuit)
 		}
 	}
 	return d
 }
 
-func deal(d deck, handSize int) (deck,deck) {
-	if len(d)<handSize {
-		return deck{},d
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
+func readFromFile(filename string) deck {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
 	}
-	return d[handSize:],d[:handSize]
+	return deck(strings.Split(string(b), ","))
+
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	if len(d) < handSize {
+		return deck{}, d
+	}
+	return d[handSize:], d[:handSize]
 }
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 func main() {
-	cards := newDeck()
-	cards, hand := deal(cards, 5)
-	hand.print()
-	cards.print()
+	// cards := newDeck()
+	cards := readFromFile("first_deck.txt")
 
+	// cards.saveToFile("first_deck.txt")
+	// cards, hand := deal(cards, 5)
+	// hand.print()
+	cards.print()
 
 }
